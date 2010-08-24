4e0a0e35f1b28fbe01dba7eae90c256e2c30135b
Andrew Jeffery <andrew@aj.id.au>
2010-08-24 20:27:39 +0930

    Don't try to free() non-malloc'ed memory after returning.
    Someone hadn't drunk enough coffee.

--- src/libiview/get_index.c
+++ src/libiview/get_index.c
@@ -6,5 +6,4 @@
 ssize_t iv_get_index(struct iv_config *config, char **buf_ptr) {
     config->api.query = "seriesIndex";
     return iv_get_xml_buffer(&(config->api), buf_ptr);
-    free(config->api.query);
 }
