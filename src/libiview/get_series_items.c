2ac79840c7afdea2e4fee62e15e5d0d439743d75
Andrew Jeffery <andrew@aj.id.au>
2010-08-27 23:40:59 +0930

    iv_get_series_items: Don't free non-malloc'ed query string

--- src/libiview/get_series_items.c
+++ src/libiview/get_series_items.c
@@ -12,7 +12,7 @@ ssize_t iv_get_series_items(struct iv_config *config, char *uri,
 #define QUERY_BUF_SZ 14
     char query_buf[QUERY_BUF_SZ];
     snprintf(query_buf, QUERY_BUF_SZ, "id=%d", series->id);
-    series_uri.query = query_buf;
+    series_uri.query = strdup(query_buf);
     int result = iv_get_xml_buffer(&series_uri, buf_ptr);
     ne_uri_free(&series_uri);
     return result;
