fa5b996d8d2fd12dd981c5a72e3dc259046eb832
Andrew Jeffery <andrew@aj.id.au>
2011-01-11 23:34:41 +1030

    iv_get_index: Append query separator to API URL as necessary

--- libiview/get_index.c
+++ libiview/get_index.c
@@ -3,12 +3,15 @@
 #undef _GNU_SOURCE
 #include <stdlib.h>
 #include <string.h>
+#include <libxml/xmlstring.h>
 #include "iview.h"
 
 ssize_t iv_get_index(struct iv_config *config, char **buf_ptr) {
     char *api_query;
     // FIXME: GNU magic sprintf, not portable.
-    if(-1 == asprintf(&api_query, "%s?%s", config->api, "seriesIndex")) {
+    if(-1 == asprintf(&api_query, "%s%s%s", config->api,
+                '?' == config->api[xmlStrlen(config->api)-1] ? "" : "?",
+                "seriesIndex")) {
         return -IV_ENOMEM;
     }
     int result = iv_get_xml_buffer(api_query, buf_ptr);
