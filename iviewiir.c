e1181602af7f652d2495d5549cc8fd202389a863
Joel Stanley <joel@jms.id.au>
2011-05-14 15:36:24 +0930

    Add copyright header to iviewiir.c

--- iviewiir.c
+++ iviewiir.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011 Joel Stanley <joel@jms.id.au>
+ * Copyright 2011 Andrew Jeffery <andrew@aj.id.au>
+ *
+ * This file is part of iviewiir.
+ *
+ * iviewiir is free software: you can redistribute it and/or modify it under
+ * the terms of version 3 the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * iviewiir is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iviewiir.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
