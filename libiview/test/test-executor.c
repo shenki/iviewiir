5e3e72b85f5d6838b431cf63100afb4abc0f0ee8
Andrew Jeffery <andrew@aj.id.au>
2011-05-21 17:37:36 +0930

    libiview: Add test harness and suite for strn?rpl()
    CuTest has been included in libiview/test and is used for unit testing
    strn?rpl() for the moment. Previous commits have been building up to
    making libiview more testable (specifically breaking apart the
    authentication functions).

---
+++ libiview/test/test-executor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "CuTest.h"
+
+#ifndef LIBIVIEW_TEST
+#error "libiview's objects must be compiled with -DLIBIVIEW_TEST to link test-executor corrctly"
+#endif
+
+CuSuite *strrpl_get_cusuite();
+
+int main() {
+    CuString *output = CuStringNew();
+    CuSuite *suite = CuSuiteNew();
+    CuSuiteAddSuite(suite, strrpl_get_cusuite());
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+    printf("%s\n", output->buffer);
+    return 0;
+}
